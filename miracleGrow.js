3c3f30642084cf9aef002154d176edc64ac1b553
JackDunne <8jdunne@stpatrick.org>
2017-11-17 14:39:50 -0600

    Add files via upload

--- miracleGrow.js
+++ miracleGrow.js
@@ -5,10 +5,14 @@ const Tree = require('./Tree.js');
 const MiracleGrow = function(size){
     let forestSize = size;
     let plantingSeeds = new Queue();
-    for(let x = 0;x<forestsize;x++){
-        plantingSeeds.enqueue(random().discreteRangeIn(1,40));
+    for(let x = 0;x<forestSize;x++){
+        plantingSeeds.enqueue(Random().discreteRangeIn(1,1000));
     }
-    let forest = new Tree();
-
-
+    let forest = new Tree(0);
+    for(let x = 0;x<plantingSeeds.length;x++){
+         forest.add(plantingSeeds.dequeue());
+   }
+   return forest;
 }
+module.exports = MiracleGrow;
+MiracleGrow(1000);
